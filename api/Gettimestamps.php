2dd8d2916e8c6cc1e696f62428bab9b092f8866a
iot <iot@raspberrypi>
2017-12-22 10:20:34 +0100

    some more update

---
+++ api/Gettimestamps.php
@@ -0,0 +1,21 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+require_once("DatabaseScript.php");
+$userid = $_POST["userid"];
+$starttimestamp = $_POST["starttimestamp"];
+$endtimestamp = $_POST["starttimestamp"];
+//'2017-12-22 09:07:36'
+
+$sql = "SELECT * FROM users, timestamps WHERE users.id = timestamps.userid AND timestamp >'$starttimestamp' AND timestamp < '$endtimestamp'";
+$result = mysqli_query($link, $sql) or die("Fout bij uitvoeren query");
+$rows = array();
+while($r = mysqli_fetch_assoc($result)) 
+{
+    $rows[] = $r;
+}
+print json_encode($rows)
+
+?>
--- script.php
+++
@@ -1,5 +0,0 @@
-<?php
-$data1 = $_POST["data1"];
-$data2 = $_POST["data2"];
-print $data1 . " " . $data2;
-?>
